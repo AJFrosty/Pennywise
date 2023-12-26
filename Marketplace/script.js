8d2b2c9717392fb4a6783ca6e195f8ea0ff74210
HeldEnderMen <ajantefraser13@gmail.com>
2023-12-26 11:13:39 -0400

    CSS Editing

--- Marketplace/script.js
+++ Marketplace/script.js
@@ -1,16 +1,16 @@
 const businesses = [
-    { category: "clothing", name: "Clothing Business 1", logo: "img/clothing1.jpeg", link: "#" },
-    { category: "clothing", name: "Clothing Business 2", logo: "img/clothing2.jpeg", link: "#" },
-    { category: "clothing", name: "Clothing Business 3", logo: "img/clothing3.jpeg", link: "#" },
-    { category: "technology", name: "Tech Business 1", logo: "img/tech1.jpeg", link: "#" },
-    { category: "technology", name: "Tech Business 2", logo: "img/tech2.jpeg", link: "#" },
-    { category: "technology", name: "Tech Business 3", logo: "img/tech3.jpeg", link: "#" },
-    { category: "cosmetic", name: "Cosmetic Business 1", logo: "img/cos1.jpeg", link: "#" },
-    { category: "cosmetic", name: "Cosmetic Business 2", logo: "img/cos2.jpeg", link: "#" },
-    { category: "cosmetic", name: "Cosmetic Business 3", logo: "img/cos3.jpeg", link: "#" },
-    { category: "groceries", name: "Grocery Business 1", logo: "img/grocery1.jpeg", link: "#" },
-    { category: "groceries", name: "Grocery Business 2", logo: "img/grocery2.jpeg", link: "#" },
-    { category: "groceries", name: "Grocery Business 3", logo: "img/grocery3.jpeg", link: "#" },
+    { category: "clothing", name: "Marty's Clothing Store", logo: "img/clothing1.jpeg", link: "#" },
+    { category: "clothing", name: "Fashion Shop", logo: "img/clothing2.jpeg", link: "#" },
+    { category: "clothing", name: "Wandery Jeans", logo: "img/clothing3.jpeg", link: "#" },
+    { category: "technology", name: "Girl Tech", logo: "img/tech1.jpeg", link: "#" },
+    { category: "technology", name: "Creative Technology", logo: "img/tech2.jpeg", link: "#" },
+    { category: "technology", name: "Hexagon", logo: "img/tech3.jpeg", link: "#" },
+    { category: "cosmetic", name: "Beauty Bee", logo: "img/cos1.jpeg", link: "#" },
+    { category: "cosmetic", name: "Beauty By Misha", logo: "img/cos2.jpeg", link: "#" },
+    { category: "cosmetic", name: "Cosmeteco", logo: "img/cos3.jpeg", link: "#" },
+    { category: "groceries", name: "Shop Grocery", logo: "img/grocery1.jpeg", link: "#" },
+    { category: "groceries", name: "Organic Grocery", logo: "img/grocery2.jpeg", link: "#" },
+    { category: "groceries", name: "Fruit Store", logo: "img/grocery3.jpeg", link: "#" },
 ];
 
 // Function to create a business element
